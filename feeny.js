9584c722d3dde51306749195aa761a906cb9fce5
Simon Townsend <simon.townsend1@ge.com>
2017-07-30 12:50:03 -0500

    add 'test' and 'mock' commands to separate functionality

--- feeny.js
+++ feeny.js
@@ -8,73 +8,91 @@ const EnvManager = require('./lib/envManager');
 const TestManager = require('./lib/testManager');
 const MockServer = require('./lib/mockServer');
 let mockServer;
-let program = require('commander');
+let Commander = require('commander');
 
-program
+Commander
   .version('0.1.0')
-  .option('-d, --directory <directory>', 'Test Directory. defaults to integration-tests/')
+
+Commander
+  .command('test')
+  .description('execute tests')
+  .option('-d, --directory <directory>', 'Test Directory. defaults to feeny/')
   .option('-c, --config <config>', 'Config File. defaults to config.json. parsed as being relative to --directory')
   .option('-D, --debug', 'debug mode')
   .option('-s, --skipEnvProvisioning', 'Will skip provisioning of environments for each Test Set. Assumes envs are already running')
-  .option('-m, --mockServer', 'Run the mock server')
-  .parse(process.argv);
+  .action((options) => {
+    const conf = parseConfiguration(options);
+    initTests(conf);
+  });
 
-const DEBUG = process.env.DEBUG || program.debug;
+Commander
+  .command('mock')
+  .description('runs a mock REST API server using your tests as mocks')
+  .option('-d, --directory <directory>', 'Test Directory. defaults to feeny/')
+  .option('-c, --config <config>', 'Config File. defaults to config.json. parsed as being relative to --directory')
+  .action((options) => {
+    const conf = parseConfiguration(options);
+    let mockServer = new MockServer(conf);
+  });
 
-// 1. parse config and merge in any useful command line args
-let parsedConf = parseFiles.parse(program);
-if (!parsedConf) {
-  throw new Exception("No config.json found");
-}
+Commander.parse(process.argv);
 
-const conf = Object.assign({}, parsedConf, {
-  cmdOpts: program,
-  debug: DEBUG
-});
+function parseConfiguration(cmdOpts) {
+  const DEBUG = process.env.DEBUG || cmdOpts.debug;
 
-if (conf.cmdOpts.mockServer) {
-  mockServer = new MockServer(conf);
-};
-
-// 2. instantiate EnvManager and ApiManager. handle shutdown signals
-let restManager = new RESTManager(conf);
-let envManager = new EnvManager(conf, restManager);
-let testManager = new TestManager(conf, envManager, restManager);
-
-async function shutdown(err) {
-  if (err)
-    console.log(err);
-
-  //envManager.stopAll().then(() => process.exit(0));
-  try {
-    await envManager.stopAll();
-  } catch (e) {
-    console.log(e);
+  // 1. parse config and merge in any useful command line args
+  let parsedConf = parseFiles.parse(cmdOpts, DEBUG);
+  if (!parsedConf) {
+    throw new Exception("No config.json found");
   }
 
-  process.exit(0);
+  return Object.assign({}, parsedConf, {
+    cmdOpts: cmdOpts,
+    debug: DEBUG
+  });
 }
 
-process.on('uncaughtException', (err) => {
-  shutdown(err);
-});
 
-process.on('SIGHUP', (err) => {
-  shutdown(err);
-});
+function initTests(conf) {
+  // 2. instantiate EnvManager and ApiManager. handle shutdown signals
+  let restManager = new RESTManager(conf);
+  let envManager = new EnvManager(conf, restManager);
+  let testManager = new TestManager(conf, envManager, restManager);
 
-process.on('SIGINT', (err) => {
-  shutdown(err);
-});
+  function shutdown(err) {
+    if (err)
+      console.log(err);
 
-return
-testManager.buildTestSetTasks();
+    envManager.stopAll()
+      .then(() => {
+        process.exit(0)
+      })
+      .catch((err) => {
+        console.log(err);
+        process.exit(1);
+      });
+  }
 
-// spin up testSetTasks in parallel and then run tests
-let parallelism = 1;
-if (conf.env && conf.env.parallelism)
-  parallelism = conf.env.parallelism
+  process.on('uncaughtException', (err) => {
+    shutdown(err);
+  });
 
-_async.parallelLimit(testManager.testSetTasks, parallelism, (err, results) => {
-  console.log(err || JSON.stringify(results, null, '\t'));
-});
+  process.on('SIGHUP', (err) => {
+    shutdown(err);
+  });
+
+  process.on('SIGINT', (err) => {
+    shutdown(err);
+  });
+
+  testManager.buildTestSetTasks();
+
+  // spin up testSetTasks in parallel and then run tests
+  let parallelism = 1;
+  if (conf.env && conf.env.parallelism)
+    parallelism = conf.env.parallelism
+
+  _async.parallelLimit(testManager.testSetTasks, parallelism, (err, results) => {
+    console.log(err || JSON.stringify(results, null, '\t'));
+  });
+}
