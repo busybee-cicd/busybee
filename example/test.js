945fdfc2400f7fcbc2b70f19a16a05753d0d0846
Simon Townsend <simon.townsend1@ge.com>
2017-12-11 09:55:36 -0600

    readme updates, example updates, init files

---
+++ example/test.js
@@ -0,0 +1,38 @@
+module.exports = [{
+    "name": "my test",
+    "skip": false,
+    "delay": 0,
+    "testSet": {
+        "id": "My Test Set",
+        "index": 1
+    },
+    "request": {
+        "method": "GET",
+        "endpoint": "/my-endpoint",
+        "query": {
+            "myQueryParam": "myQueryValue"
+        },
+        "body": {
+            "myBodyKey": "myBodyValue"
+        }
+    },
+    "expect": {
+        "status": 200,
+        "headers": {
+            "my-expected-header": "myExpectedHeaderValue"
+        },
+        "body": (body) => {
+            try {
+                return body.content.hello === 'world' ? true : false;
+            } catch (e) {
+                return false;
+            }
+
+        }
+    },
+    "mockResponse": {
+        "content": {
+            "hello": "world"
+        }
+    }
+}]
