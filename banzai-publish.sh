44dc35311c3755c947eec10fc41a89aa2d502d1c
Simon Townsend <simon.townsend1@ge.com>
2018-07-10 12:01:44 -0500

    publishScript additions

--- banzai-publish.sh
+++ banzai-publish.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 echo "building documentation"
+source /var/jenkins_home/.nvm/nvm.sh
+nvm use 8
 git config --global user.email "Service.SweeneyJenkins@ge.com"
 git config --global user.name "Sweeny Jenkins"
 git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
 git fetch origin
-git checkout -b gh-pages origin/gh-pages
+#git checkout gh-pages origin/gh-pages
+git checkout gh-pages
 git merge master
 npm run docs
 git add .
