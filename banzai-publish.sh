326d6c7ddff7f23f2630b6fc8e68963a79d6739a
Simon Townsend <simon.townsend1@ge.com>
2018-07-10 11:49:42 -0500

    add git info to publishScript

--- banzai-publish.sh
+++ banzai-publish.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 echo "building documentation"
+git config --global user.email "Service.SweeneyJenkins@ge.com"
+git config --global user.name "Sweeny Jenkins"
+git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
 git fetch origin
 git checkout -b gh-pages origin/gh-pages
 git merge master
