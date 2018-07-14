2866aa91bc762bfd0d58125e2b19222aeecab9b2
Simon Townsend <simon.townsend1@ge.com>
2018-07-14 11:39:21 -0500

    dump yarn..just use npm

--- banzai-build.sh
+++ banzai-build.sh
@@ -5,22 +5,11 @@ source /var/jenkins_home/.nvm/nvm.sh
 nvm use 8
 npm config set proxy "http://iss-americas-pitc-cincinnatiz.proxy.corporate.ge.com:80"
 npm config set https-proxy "http://iss-americas-pitc-cincinnatiz.proxy.corporate.ge.com:80"
-
-command -v yarn >/dev/null 2>&1 || {
-    echo "Installing Yarn..."
-    npm install -g yarn;
-}
-
-command -v mockserver >/dev/null 2>&1 || {
-    echo "Installing mockserver..."
-    yarn global add mockserver;
-}
+npm install -g mockserver;
 
 echo "Installing Dependencies"
-yarn install
+npm install
 echo "Running Unit Tests" 
 npm test
 echo "Running IT"
-export NO_PROXY=localhost
 npm run IT
-unset NO_PROXY
