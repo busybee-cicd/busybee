86afccef800f89831a755c0d5284d5bb679909dd
Gaffney, Robert <robert.gaffney@ge.com>
2020-01-06 14:21:07 -0600

    add async/await where missing

--- src/managers/RESTSuiteManager.ts
+++ src/managers/RESTSuiteManager.ts
@@ -250,7 +250,7 @@ export class RESTSuiteManager {
     }
   }
 
-  wait(milliseconds) {
+  async wait(milliseconds) {
     this.logger.debug(`wait ${milliseconds / 1000} second(s)`);
     return new Promise(resolve => setTimeout(resolve, milliseconds));
   }
@@ -459,7 +459,7 @@ export class RESTSuiteManager {
         // Run Custom Function Assertion OR basic Pojo comparision
         if (_.isFunction(test.expect.body)) {
           // if the test has a custom function for assertion, run it.
-          let bodyResult = test.expect.body(actual, testSet.variableExports);
+          let bodyResult = await test.expect.body(actual, testSet.variableExports);
           if (bodyResult === false) {
             bodyPass = false;
           } // else we pass it. ie) it doesn't return anything we assume it passed.
